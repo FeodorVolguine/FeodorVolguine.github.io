5f7afe70f4fafea81a1d8f0b08da8f49c396adb5
FeodorVolguine <email17329@gmail.com>
2021-12-01 00:00:53 -0500

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,4 @@
+//const navToggle = document.getElementById('nav-toggle');
+//const nav = document.getElementById('nav');
+
+//navToggle.addEventListener('click', () => { nav.classList.toggle('active'); });
