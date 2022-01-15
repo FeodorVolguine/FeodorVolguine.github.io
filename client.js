20fca192347f21dba0600e7127626d377200acfd
FeodorVolguine <email17329@gmail.com>
2022-01-14 23:02:19 -0500

    Create color palette in SASS, implement nav toggle button, and add placeholder text

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-//const navToggle = document.getElementById('nav-toggle');
-//const nav = document.getElementById('nav');
+const navToggle = document.getElementById('nav-toggle');
+const nav = document.getElementById('nav');
 
-//navToggle.addEventListener('click', () => { nav.classList.toggle('active'); });
+navToggle.addEventListener('click', () => { nav.classList.toggle('active'); });
