9efd2bed19c1abae38abe4c7c6edef0283f20a1f
FeodorVolguine <email17329@gmail.com>
2022-02-21 18:39:55 -0500

    Create draggable work experience cards

--- client.js
+++ client.js
@@ -2,3 +2,35 @@ const navToggle = document.getElementById('nav-main-toggle');
 const nav = document.getElementById('nav-main');
 
 navToggle.addEventListener('click', () => { nav.classList.toggle('active'); });
+
+const workExperienceCards = document.getElementById('workExperienceCards');
+
+let currentScrollPosition = { x: 0, y: 0 };
+let mousePosition = { x: 0, y: 0 };
+
+workExperienceCards.addEventListener('mousedown', (event) =>
+{
+    workExperienceCards.style.cursor = 'grabbing';
+    workExperienceCards.style.userSelect = 'none';
+
+    currentScrollPosition = { x: workExperienceCards.scrollLeft, y: workExperienceCards.scrollTop };
+    mousePosition = { x: event.clientX, y: event.clientY };
+
+    document.addEventListener('mousemove', MouseMoveEvent);
+    document.addEventListener('mouseup', MouseUpEvent);
+});
+
+function MouseMoveEvent(event)
+{
+    workExperienceCards.scrollLeft = currentScrollPosition.x - (event.clientX - mousePosition.x);
+    workExperienceCards.scrollTop = currentScrollPosition.y - (event.clientY - mousePosition.y);
+}
+
+function MouseUpEvent()
+{
+    workExperienceCards.style.cursor = 'grab';
+    workExperienceCards.style.removeProperty('user-select');
+
+    document.removeEventListener('mousemove', MouseMoveEvent);
+    document.removeEventListener('mouseup', MouseUpEvent);
+}
